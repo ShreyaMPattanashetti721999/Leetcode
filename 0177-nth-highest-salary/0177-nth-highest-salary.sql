df441df42582aa40c8bd984f6889634d4a4bf18b
Shreya Pattanashetti <135322207+ShreyaMPattanashetti721999@users.noreply.github.com>
2026-01-16 22:04:37 +0000

    Time: 419 ms (55.98%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,22 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+SET N = N - 1;
   RETURN (
-    SELECT
-      CASE
-        WHEN N <= 0 THEN NULL
-        WHEN COUNT(DISTINCT salary) < N THEN NULL
-        ELSE (
-          SELECT salary
-          FROM (
-            SELECT DISTINCT salary
-            FROM Employee
-            ORDER BY salary DESC
-            LIMIT N
-          ) t
-          ORDER BY salary ASC
-          LIMIT 1
-        )
-      END
-    FROM Employee
+      
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+      LIMIT 1 OFFSET N
   );
-END;
+END
